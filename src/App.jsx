591b1979dab9af806b86b3a826b69b8c69723008
aripzuan <arridz03@gmail.com>
2025-07-20 20:58:36 +0800

    inital commit for DoIt.AI

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Home  from './pages/Home';
+import Community from './pages/Community';
+import  Dashboard from './pages/Dashboard';
+import Layout from './pages/Layout';
+import RemoveObj  from './pages/RemoveObj';
+import ReviewResume  from './pages/ReviewResume';
+import WriteArticle from './pages/WriteArticle';
+import BlogTitles from './pages/BlogTitles';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/ai' element={<Layout/>}>
+          <Route index element={<Dashboard/>} />
+          <Route path='write-article' element={<WriteArticle/>} />
+          <Route path='blog-titles' element={<BlogTitles/>} />
+          <Route path='community' element={<Community/>} />
+          <Route path='remove-object' element={<RemoveObj/>} />
+          <Route path='review-resume' element={<ReviewResume/>} />
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/RemoveObj.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RemoveObj = () => {
+  return (
+    <div><h1>Remove Object</h1></div>
+  )
+}
+
+export default RemoveObj
